c2f572c414ebc9f9c8e083341734a3ce843d28ca
Claudia <claudiacampanella.w@gmail.com>
2025-10-06 15:26:05 +0200

    Initial commit: Knowledge Graph Service

---
+++ src/test/java/com/cld/knowledge_graph_service/KnowledgeGraphServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cld.knowledge_graph_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeGraphServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
